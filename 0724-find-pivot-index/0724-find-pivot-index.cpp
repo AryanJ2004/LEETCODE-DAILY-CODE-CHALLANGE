b526bd998247a5d41d8d7ccc57c0e68153487d06
Aryan Jain <aryanj1084@gmail.com>
2025-06-12 12:43:33 +0530

    Time: 0 ms (100%), Space: 35.8 MB (51.05%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int tot_sum=0;
+        for(auto i:nums){
+            tot_sum+=i;
+        }
+
+        int cur_sum=0;
+        for(int i=0;i<nums.size();i++){
+            cur_sum+=nums[i];
+
+            int l_sum=cur_sum-nums[i];
+            int r_sum=tot_sum-cur_sum;
+
+            if(l_sum==r_sum){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
