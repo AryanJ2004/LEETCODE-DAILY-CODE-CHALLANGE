293ecfe1a6b08d839012db622b2c432e5699eb91
Aryan Jain <aryanj1084@gmail.com>
2025-07-06 12:30:03 +0530

    Time: 0 ms (100%), Space: 15.2 MB (66.13%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int si=0;
-        int ei=nums.size()-1;
-        int ans=search(nums,si,ei,target);
-        return ans;
-    }
+        int low=0,high=nums.size()-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]==target) return mid;
 
-    int search(vector<int>vec,int si,int ei,int tar)
-    {
-        if(si>ei)
+            if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && nums[mid]>=target){
+                    high=mid-1;
+                }else{
+                    low=mid+1;
+                }
+            }else{
+                if(nums[mid]<=target && nums[high]>=target){
+                    low=mid+1;
+                }else{
+                    high=mid-1;
+                }
+            }
+        }
         return -1;
-        
-        int mid=si+(ei-si)/2;
-        if(vec[mid]==tar)
-        return mid;
-        if(vec[si]<=vec[mid])
-        {
-            if(vec[si]<=tar && tar<=vec[mid])
-            return search(vec,si,mid-1,tar);
-            else
-            return search(vec,mid+1,ei,tar);
-        }
-        else
-        {
-            if(vec[mid]<=tar&& tar<=vec[ei])
-            return search(vec,mid+1,ei,tar);
-            else
-            return search(vec,si,mid-1,tar);
-        }
     }
 };
