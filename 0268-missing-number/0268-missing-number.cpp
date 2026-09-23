51afb4043e6725482680ca3aa9a03972d5477424
Aryan Jain <aryanj1084@gmail.com>
2026-09-23 20:49:01 +0530

    Time: 0 ms (100%), Space: 21.7 MB (70.75%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+        int sum=accumulate(nums.begin(),nums.end(),0);
         int n=nums.size();
-        int Sall=((n*(n+1))/2);
-        auto first=nums.begin();
-        auto end=nums.end();
-        int sumarr=accumulate(first,end,0);
-        return Sall-sumarr;
+        int totSum=(n*(n+1))/2;
+        return totSum-sum;
     }
 };
